2d24e4ba613bbfc0443251d62cda2d0a1642f49f
mo-cherif <mocherifh0@gmail.com>
2023-10-04 11:12:37 +0100

    .

--- utils/dragAndDrop.js
+++ utils/dragAndDrop.js
@@ -6,17 +6,22 @@ export const handleDragOver = (e) => {
   e.preventDefault();
 };
 
-export const handleDrop = (e, setImage, setBorder) => {
+export const handleDrop = (e, setImage, setBorder, setLoading) => {
   e.preventDefault();
   const file = e.dataTransfer.files[0];
-  if (file && file.type.startsWith("image/")) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      setImage(e.target.result);
-      setBorder(false);
-    };
 
-    reader.readAsDataURL(file);
+  if (file && file.type.startsWith("image/")) {
+    setLoading(false);
+    setTimeout(() => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setImage(e.target.result);
+        setBorder(false);
+        setLoading(true);
+      };
+
+      reader.readAsDataURL(file);
+    }, 2000);
   } else {
     alert("Please drop an image file.");
   }
