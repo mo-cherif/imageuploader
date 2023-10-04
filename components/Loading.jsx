3c4592a205732e29252b4a19433d9a543bdf2b22
mo-cherif <mocherifh0@gmail.com>
2023-10-04 11:54:29 +0100

    loading component done with beautiful animation

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,5 +1,18 @@
+import { poppins } from "@/utils/fonts";
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div
+      className={`${poppins.className} h-screen w-screen flex justify-center items-center`}
+    >
+      <div className="flex w-[400px] h-[144px] justify-center p-10 rounded-xl flex-col gap-6 shadow-3xl">
+        <h1 className="font-medium text-[14px]">Uploading...</h1>
+        <div className="w-[310px] h-[6px] bg-[#F2F2F2]">
+          <div className="w-[80px] load relative  h-[6px] rounded-md bg-[#2F80ED]"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
