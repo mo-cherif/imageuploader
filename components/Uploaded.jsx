736840112659697defbbc8be57ab7fd43bce2ea4
mo-cherif <mocherifh0@gmail.com>
2023-10-04 13:26:46 +0100

    the copy button is working

--- components/Uploaded.jsx
+++ components/Uploaded.jsx
@@ -5,6 +5,11 @@ import { useState } from "react";
 const Uploaded = ({ icon }) => {
   const [value, setValue] = useState("https://imageUpload/photo-388888883");
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(value);
+    alert("Copied the text: " + value);
+  };
+
   return (
     <div className="p-7 flex flex-col gap-2 justify-center items-center">
       <h1 className="text-4xl">☑️</h1>
@@ -27,7 +32,10 @@ const Uploaded = ({ icon }) => {
                 type="text"
               />
             </div>
-            <button className="bg-[#2F80ED] text-[8px] rounded-[8px] font-medium w-[74px] text-[#FFF]">
+            <button
+              onClick={handleCopy}
+              className="bg-[#2F80ED] text-[8px] rounded-[8px] font-medium w-[74px] text-[#FFF]"
+            >
               Copy Link
             </button>
           </div>
