5785c0b425771dd9741be16bf0fc7b8465a7a0b5
mo-cherif <mocherifh0@gmail.com>
2023-10-05 19:44:39 +0100

    cloudianry

--- utils/dragAndDrop.js
+++ utils/dragAndDrop.js
@@ -6,11 +6,20 @@ export const handleDragOver = (e) => {
   e.preventDefault();
 };
 
+const fetchData = async (file) => {
+  const data = await fetch(
+    `https://api.cloudinary.com/v1_1/dnlqnina8/image/upload`
+  ).then((response) => response.json());
+
+  console.log("data : ", data);
+};
+
 export const handleDrop = (e, setImage, setLoading, setPhotoName) => {
   e.preventDefault();
   const file = e.dataTransfer.files[0];
 
   if (file && file.type.startsWith("image/")) {
+    fetchData(file);
     setPhotoName(`htpp://imageupload/${file.name}`);
     setLoading(false);
     setTimeout(() => {
