bd1b291c25a12ebf53eb4e22641506ad8547075c
mo-cherif <mocherifh0@gmail.com>
2023-10-05 20:55:46 +0100

    the v1 is ready

--- utils/dragAndDrop.js
+++ utils/dragAndDrop.js
@@ -6,31 +6,31 @@ export const handleDragOver = (e) => {
   e.preventDefault();
 };
 
-const fetchData = async (file) => {
-  const data = await fetch(
-    `https://api.cloudinary.com/v1_1/dnlqnina8/image/upload`
-  ).then((response) => response.json());
-
-  console.log("data : ", data);
-};
-
-export const handleDrop = (e, setImage, setLoading, setPhotoName) => {
+export const handleDrop = async (e, setImage, setLoading, setPhotoName) => {
   e.preventDefault();
   const file = e.dataTransfer.files[0];
+  setLoading(false);
 
   if (file && file.type.startsWith("image/")) {
-    fetchData(file);
-    setPhotoName(`htpp://imageupload/${file.name}`);
-    setLoading(false);
-    setTimeout(() => {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setImage(e.target.result);
-        setLoading(true);
-      };
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "image_uploader");
 
-      reader.readAsDataURL(file);
-    }, 2000);
+    const data = await fetch(
+      `https://api.cloudinary.com/v1_1/dnlqnina8/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    ).then((res) => res.json());
+    setPhotoName(data.secure_url);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setImage(e.target.result);
+      setLoading(true);
+    };
+    reader.readAsDataURL(file);
   } else {
     alert("Please drop an image file.");
   }
